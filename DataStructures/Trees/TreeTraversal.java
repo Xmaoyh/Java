0abb1bdac8b2375552ba0eb381a94edb6e5ca3b7
maoyihan <maoyihan123@163.com>
2021-07-06 15:59:22 +0800

    树的注释

--- DataStructures/Trees/TreeTraversal.java
+++ DataStructures/Trees/TreeTraversal.java
@@ -7,6 +7,13 @@ import java.util.LinkedList;
 // Driver Program
 public class TreeTraversal {
   public static void main(String[] args) {
+    /**
+     * ⁠   5
+     * ⁠  / \
+     * ⁠ 3    7
+     * / \  / \
+     * 2 4  6  8
+     */
     Node tree = new Node(5);
     tree.insert(3);
     tree.insert(2);
